aa18f0017dc9de6395b0aa3d82bf18f44019e24e
Thomas-Joseph-Ryan <tjryan13579@gmail.com>
2023-04-07 20:35:23 +1000

    Finished server part (without testing)

--- COMP3221_FLServer.py
+++ COMP3221_FLServer.py
@@ -4,29 +4,67 @@ import torch
 import torch.nn as nn
 from MultinominalLogisticRegression import MultinominalLogisticRegression
 import socket
+import threading
+import signal
+import queue
+import time
+import random
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
 
-def aggregate_models(client_models, global_model: MultinominalLogisticRegression) -> MultinominalLogisticRegression:
-    num_clients = len(client_models)
+stop_event = threading.Event()
 
-    # Initialize the aggregated weights and biases to zero
-    aggregated_weights = torch.zeros_like(client_models[0].linear.weight)
-    aggregated_biases = torch.zeros_like(client_models[0].linear.bias)
+clients: dict[int, dict[str, str | int]] = {}
 
-    # Sum the weights and biases from all client models
-    for model in client_models:
-        aggregated_weights += model.linear.weight
-        aggregated_biases += model.linear.bias
+inbound_information = queue.Queue()
 
-    # Calculate the average of the weights and biases
-    aggregated_weights /= num_clients
-    aggregated_biases /= num_clients
+SUB_CLIENT_NUMBER = 2
 
-    # Update the global model with the averaged weights and biases
-    global_model.linear.weight.data = aggregated_weights
-    global_model.linear.bias.data = aggregated_biases
 
+def aggregate_models(client_models: list[tuple[MultinominalLogisticRegression, int]], 
+                     global_model: MultinominalLogisticRegression,
+                     sub_client: int) -> MultinominalLogisticRegression:
+    if sub_client == 0 or len(client_models) < SUB_CLIENT_NUMBER:
+        num_clients = len(client_models)
+
+        # Initialize the aggregated weights and biases to zero
+        aggregated_weights = torch.zeros_like(client_models[0][0].linear.weight)
+        aggregated_biases = torch.zeros_like(client_models[0][0].linear.bias)
+
+        # Sum the weights and biases from all client models
+        for model in client_models:
+            aggregated_weights += model[0].linear.weight
+            aggregated_biases += model[0].linear.bias
+
+        # Calculate the average of the weights and biases
+        aggregated_weights /= num_clients
+        aggregated_biases /= num_clients
+
+        # Update the global model with the averaged weights and biases
+        global_model.linear.weight.data = aggregated_weights
+        global_model.linear.bias.data = aggregated_biases
+
+    elif sub_client == 1 and len(client_models) >= SUB_CLIENT_NUMBER:
+        num_clients = SUB_CLIENT_NUMBER
+
+        # Initialize the aggregated weights and biases to zero
+        aggregated_weights = torch.zeros_like(client_models[0][0].linear.weight)
+        aggregated_biases = torch.zeros_like(client_models[0][0].linear.bias)
+
+        # Choose 2 client models at random
+        selected_client_models = random.sample(client_models, num_clients)
+        # Sum the weights and biases from all client models
+        for model in selected_client_models:
+            aggregated_weights += model[0].linear.weight
+            aggregated_biases += model[0].linear.bias
+
+        # Calculate the average of the weights and biases
+        aggregated_weights /= num_clients
+        aggregated_biases /= num_clients
+
+        # Update the global model with the averaged weights and biases
+        global_model.linear.weight.data = aggregated_weights
+        global_model.linear.bias.data = aggregated_biases
     return global_model
 
 
@@ -67,19 +105,127 @@ def start():
     # Your server implementation goes here
     input_size = 28 * 28  # for 28x28 pixel images
     num_classes = 10  # for digits 0 to 9
-    model = MultinominalLogisticRegression(input_size, num_classes)
+    global_model = MultinominalLogisticRegression(input_size, num_classes)
 
+    listen_thread = threading.Thread(target=thread_listen, args=(port_server,))
+    listen_thread.start()
+
+    while True:
+        # Wait indefinately for client to join
+        inbound_info = inbound_information.get(block=True)
+        message, client_port = inbound_info[0], inbound_info[1]
+        if message.split(" ")[0] == "hello":
+            add_client(message, client_port)
+        else: 
+            continue
+
+        # Once a single client joins, 30 second timer begins
+        start_time = time.time()
+        timeout = 30  
+        while True:
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+
+            if elapsed_time >= timeout:
+                break
+
+            try:
+                inbound_info = inbound_information.get(block=True, timeout=0.5)
+                message, client_port = inbound_info[0], inbound_info[1]
+                if message.split(" ")[0] == "hello":
+                    add_client(message, client_port)
+            except queue.Empty:
+                continue
+        break
+
+    iteration = 1
+    iteration_data = []
+    broadcast_model(global_model)
+    while iteration <= 100:
+        print(f"Global Iteration {iteration}:")
+        num_clients = len(clients)
+        print(f"Total Number of clients: {num_clients}")
+        while True:
+            inbound_info = inbound_information.get(block=True)
+            message, client_port = inbound_info[0], inbound_info[1]
+            if message.split(" ")[0] == "hello":
+                add_client(message, client_port)
+            else:
+                client_data = clients[client_port]
+                client_id = client_data["id"]
+                print(f"Getting local model from {client_id}")
+                local_model = MultinominalLogisticRegression.deserialize(message, input_size, num_classes)
+                iteration_data.append((local_model, client_port))
+            
+            if inbound_information.qsize() == 0 and len(iteration_data) == num_clients:
+                break
+        print("Aggregating new global model")
+        global_model = aggregate_models(iteration_data, global_model, sub_client)
+        print("Broadcasting new global model\n")
+        broadcast_model(global_model)
+        
+
+def thread_listen(port: int):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('localhost', port_server))
+            s.bind(('localhost', port))
             s.listen()
-            logging.info(f"Listening on port {port_server}")
+            logging.info(f"Listening on port {port}")
 
-            while True:
-                pass
+            s.settimeout(5)
+            while not stop_event.is_set():
+
+                try:
+                    conn, (client_addr, client_port) = s.accept()
+                except socket.timeout:
+                    continue
+
+                with conn:
+                    logging.info(f"Connected by {client_port}")
+
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+
+                        data = data.decode('utf-8')
+                        logging.debug(f"Recieved data: {data}")
+                        inbound_information.put((data, client_port))
     except Exception as e:
         logging.error(f"Exception in server listening")
 
+def broadcast_model(model : MultinominalLogisticRegression):
+    data = model.serialize()
+    for client in clients:
+        client_port : int = client
+        client_data = clients[client_port]
+        client_id = client_data["id"]
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                logging.info(f"Trying to connect to {client_id} on port {client_port}")
+                s.connect(('localhost', client_port))
+                s.sendall(data)
+        except ConnectionRefusedError:
+            logging.error(f"Connection was refused by {client_id} on port {client_port}")
+        except Exception as e:
+            logging.error(f"Exception in broadcast: {e}")
+
+def add_client(hand_shake_message : str, port: str):
+    '''
+        Handshake message will be of the form
+        "hello <client_id> <data_size>" where data size is the number
+        of samples in the training data
+    '''
+    split_msg = hand_shake_message.split(" ")
+    clients[int(port)] = {"id": split_msg[1], "data_size": int(split_msg[2])}
+
+
+def quit_gracefully(signum, frame) -> None:
+    logging.info(f"Received signal {signum}, quitting gracefully...")
+    stop_event.set()
+    sys.exit(0)
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, quit_gracefully)
+    signal.signal(signal.SIGTERM, quit_gracefully)
     start()
--- MultinominalLogisticRegression.py
+++ MultinominalLogisticRegression.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import pickle
 
 class MultinominalLogisticRegression(nn.Module):
     def __init__(self, input_size, num_classes):
@@ -9,3 +10,19 @@ class MultinominalLogisticRegression(nn.Module):
     def forward(self, x):
         x = self.linear(x)
         return x
+    
+    def serialize(self):
+        model_data = {
+            "weights": self.linear.weight.data,
+            "biases": self.linear.bias.data
+        }
+        serialized_data = pickle.dumps(model_data)
+        return serialized_data
+
+    @classmethod
+    def deserialize(cls, serialized_data, input_size, num_classes):
+        model_data = pickle.loads(serialized_data)
+        model = cls(input_size, num_classes)
+        model.linear.weight.data = model_data["weights"]
+        model.linear.bias.data = model_data["biases"]
+        return model
